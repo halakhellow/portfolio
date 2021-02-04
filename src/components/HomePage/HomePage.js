d25dc7441e29c245b28c7ba92a0f7f897e8f924f
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-04 22:21:08 +0200

    add the HomePage component with its initial style

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Sections from "../Sections/Sections";
+
+import "./HomePage.css";
+
+let HomePage = () => {
+  return (
+    <div className="HomePage">
+      <h1 className="HomePage-title">Hey, It's Hala Alkhellow</h1>
+      <p></p>
+      <Sections />
+    </div>
+  );
+};
+
+export default HomePage;
