da23b50b16ea2d92616e858f353465387761ad14
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-06 00:18:51 +0200

    add Footer component with its style

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Footer.css";
+
+let Footer = () => {
+  return (
+    <div className="Footer">
+      <a href="https://github.com/halakhellow" target="_blank">
+        <i className="fab fa-github"></i>
+      </a>
+      <a href="https://linkedin.com/in/halakhellow" target="_blank">
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <a href="https://instagram.com/hall0sh" target="_blank">
+        <i className="fab fa-instagram"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
