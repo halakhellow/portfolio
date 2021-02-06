1bece41614ec7ee2661fa7ac05f3c0eb671c2fd8
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-06 23:08:30 +0200

    change Header components to be a Link from react-router-dom as well as the SectionItem component and edit its style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Header.css";
 
 let Header = () => {
   return (
-    <div className="Header">
+    <Link to="/" className="Header">
       <img src="https://img.icons8.com/ios/50/ffffff/home--v4.png" />
-    </div>
+    </Link>
   );
 };
 
--- src/components/SectionItem/SectionItem.js
+++ src/components/SectionItem/SectionItem.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./SectionItem.css";
 
 let SectionItem = ({ name, faClass }) => {
+  let linkref = name.toLowerCase();
   return (
-    <div className="SectionItem">
+    <Link to={`/${linkref}`} className="SectionItem">
       <div className="SectionItem-infos">
         <span className={`${faClass} SectionItem-icon`}></span>
         <p className="SectionItem-name">{name}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
