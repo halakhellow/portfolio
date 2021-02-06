84a77a27e94536ffa6e74747c4bb2d5c0354b4de
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-06 23:06:27 +0200

    wrap the App within BrowserRouter and add Routes for the main pages of the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import HomePage from "../src/pages/HomePage/HomePage";
+import React from "react";
+import { Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
+import ResumePage from "./pages/ResumePage/ResumePage";
+import ContactPage from "./pages/ContactPage/ContactPage";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/projects" component={ProjectsPage} />
+      <Route exact path="/resume" component={ResumePage} />
+      <Route exact path="/contact" component={ContactPage} />
     </div>
   );
 }
