53eb6952e82fd61e63e5957241b4e9296f561f44
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-06 03:26:52 +0200

    add Header component with its style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Header.css";
+
+let Header = () => {
+  return (
+    <div className="Header">
+      <img src="https://img.icons8.com/ios/50/ffffff/home--v4.png" />
+    </div>
+  );
+};
+
+export default Header;
