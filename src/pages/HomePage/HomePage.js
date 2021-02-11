28120846b7c6e153722f99b789b8b522b1949b33
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-11 23:12:53 +0200

    edit HomePage title and add some animation to it + edit the bio in the same page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -12,11 +12,19 @@ let HomePage = () => {
       <div className="page">
         <Header home />
         <h1 className="HomePage-title">
-          Hey, It's <span>Hala Alkhellow</span>
+          <div className="iamSection">Hi, I'm &nbsp;</div>
+          <div className="slideSection">
+            <div class="scroller">
+              <div class="inner">
+                <p>Hala Alkhellow</p>
+                <p>FrontEnd Developer</p>
+                <p>React Developer</p>
+              </div>
+            </div>
+          </div>
         </h1>
         <p className="HomePage-bio">
-          A React developer from Syria with proficiency in Redux, Hooks, vanilla
-          JavaScript, HTML and CSS.
+          A Frontend developer from Syria, specializing in React development.
         </p>
         <Sections />
       </div>
