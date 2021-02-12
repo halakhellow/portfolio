88c889ad5966ca33d3b294af331353a1c4da4552
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-12 22:48:13 +0200

    add PortfolioPage to display projects with its style file

---
+++ src/pages/PortfolioPage/PortfolioPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import VanillaJsApplications from "../../components/VanillaJsApplications/VanillaJsApplications";
+import ReactApplications from "../../components/ReactApplications/ReactApplications";
+
+import "./PortfolioPage.css";
+
+let PortfolioPage = () => {
+  return (
+    <div className="PortfolioPage">
+      <div className="page">
+        <Header />
+        <div className="PortfolioPage-content">
+          <h3>Applications with React.js</h3>
+          <ReactApplications />
+          <h3>Applications with vanilla JavaSript</h3>
+          <VanillaJsApplications />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default PortfolioPage;
