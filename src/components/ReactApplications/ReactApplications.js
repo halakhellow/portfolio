16c78376aef81b4d9658981e619a0b0808d1e70a
Hala Alkhellow <halakhellow1996@gmail.com>
2021-04-17 01:00:00 +0300

    add e-commerce application to ReactApplications + add the app image

--- src/components/ReactApplications/ReactApplications.js
+++ src/components/ReactApplications/ReactApplications.js
@@ -5,6 +5,7 @@ import Application from "../../components/Application/Application";
 import todoAppImg from "../../images/todoApp.png";
 import hangmanImg from "../../images/hangmanApp.png";
 import jokesAppImg from "../../images/jokesApp.png";
+import ecommerceAppImg from "../../images/ecommerceApp.png";
 
 let ReactApplications = () => {
   return (
@@ -26,6 +27,12 @@ let ReactApplications = () => {
         imageSrc={todoAppImg}
         description="App uses React forms (controlled components) + react-beautiful-dnd"
         appLink="https://halakhellow.github.io/todo-list/"
+      />
+      <Application
+        title="E COMMERCE"
+        imageSrc={ecommerceAppImg}
+        description="App uses React router, Firebase, Redux, Selectors, HOC, Stripe and Asynchronous Redux (Redux-saga)"
+        appLink="https://halakhellow.github.io/e-commerce/"
       />
     </div>
   );
