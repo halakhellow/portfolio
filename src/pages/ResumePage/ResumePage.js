418136d69aa47d5f8eca49b3216be549b845e3e5
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-13 04:43:05 +0300

    refactor: resume page with its style

--- src/pages/ResumePage/ResumePage.js
+++ src/pages/ResumePage/ResumePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "../../components/Header/Header";
+import CustomBtn from "../../components/CustomBtn/CustomBtn";
 import Footer from "../../components/Footer/Footer";
 
 import resume from "./resume.pdf";
@@ -12,11 +13,24 @@ let ResumePage = () => {
     <div className="ResumePage page">
       <div className="page-content">
         <Header />
-        <h2>Resume :</h2>
-        <iframe src={`${resume}#toolbar=0`} height="387px"></iframe>
-        <a href={resume} download className="download-button">
-          <i className="fa fa-download"></i>Download
-        </a>
+        <h2 className="ResumePage-header">Resume :</h2>
+        <p className="text">
+          Reach out to me via my{" "}
+          <a className="contact" href="/portfolio/contact">
+            contact
+          </a>
+          &nbsp; page .
+          <br />
+          View or download the resume <i class="fa fa-arrow-circle-down"></i>
+        </p>
+        <iframe
+          src={`${resume}#toolbar=0`}
+          allowfullscreen=""
+          width="500"
+          height="540"
+          title="Hala Alkhellow Resume"
+        ></iframe>
+        <CustomBtn anchorLink text="Download" />
       </div>
       <Footer />
     </div>
