7955179c82e59fbe93d551363bd012c402dc1f69
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-13 04:04:55 +0300

    add an email service in contact page and fix the form with its styles

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,27 +1,121 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import CustomBtn from "../../components/CustomBtn/CustomBtn";
 
 import "./ContactPage.css";
 
-let ContactPage = () => {
+const ContactPage = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  // Function that displays a success toast on bottom right of the page when form submission is successful
+  const toastifySuccess = () => {
+    toast("SUCCESS!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: "successMessage",
+      toastId: "notifyToast",
+    });
+  };
+
+  // Function called on submit that uses emailjs to send email of valid contact form
+  const onSubmit = async (data) => {
+    const { name, email, message } = data;
+    try {
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
+
+      await emailjs.send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_PUBLIC_KEY
+      );
+
+      reset();
+      toastifySuccess();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="ContactPage page">
       <div className="page-content">
         <Header />
         <h1>Let's Connect !</h1>
-        <form
-          action="mailto:halakhellow1996@gmail.com"
-          method="POST"
-          encType="text/plain"
-        >
-          <label htmlFor="subject"> Subject :</label>
-          <input id="subject" name="subject" type="text" />
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <label htmlFor="name"> Name :</label>
+          <div class="input-icons">
+            <i class="fa fa-user icon"></i>
+            <input
+              id="name"
+              name="name"
+              className="input-field"
+              type="text"
+              {...register("name", {
+                required: { value: true, message: "Please enter your name" },
+                maxLength: {
+                  value: 30,
+                  message: "Please use 30 characters or less",
+                },
+              })}
+            />
+          </div>
+          {errors.name && (
+            <span className="errorMessage">{errors.name.message}</span>
+          )}
+          <label htmlFor="email"> Email :</label>
+          <div class="input-icons">
+            <i class="fa fa-envelope icon"></i>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              {...register("email", {
+                required: true,
+                pattern:
+                  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              })}
+            />
+          </div>
+          {errors.email && (
+            <span className="errorMessage">
+              Please enter a valid email address
+            </span>
+          )}
           <label htmlFor="message"> Message :</label>
-          <textarea id="message" name="body"></textarea>
-          <input type="submit" value="Send" />
+          <textarea
+            id="message"
+            name="message"
+            {...register("message", {
+              required: true,
+            })}
+          ></textarea>
+          {errors.message && (
+            <span className="errorMessage">Please enter a message</span>
+          )}
+          <CustomBtn text="SEND" type="submit" />
         </form>
+
+        <ToastContainer />
       </div>
       <Footer />
     </div>
