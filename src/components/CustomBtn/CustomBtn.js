9fe1ee40f75d8b1519b914b00fbe2c5b11617c6a
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-13 04:42:36 +0300

    adjust the CustomBtn to handle the anchor tags

--- src/components/CustomBtn/CustomBtn.js
+++ src/components/CustomBtn/CustomBtn.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import resume from "../../pages/ResumePage/resume.pdf";
+
 import "./CustomBtn.css";
 
-const CustomBtn = ({ text, type }) => {
-  return (
+const CustomBtn = ({ text, type, anchorLink }) => {
+  return anchorLink ? (
+    <a className="CustomBtn" href={resume} download>
+      {text}
+    </a>
+  ) : (
     <button className="CustomBtn" type={type}>
       {text}
     </button>
