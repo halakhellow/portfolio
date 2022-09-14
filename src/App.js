ca9eeca792f13c365558dd44b9e01c8eca82af3c
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-15 01:15:53 +0300

    create global context for modal and apps in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
@@ -11,25 +11,34 @@ import ContactPage from "./pages/ContactPage/ContactPage";
 
 import "./App.css";
 
+export const ModalContext = React.createContext(null);
+export const AppContext = React.createContext(null);
+
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [appTitle, setAppTitle] = useState("");
   return (
     <Route
       render={({ location }) => (
         <TransitionGroup component="div" className="App">
-          <ShootingStars />
-          <CSSTransition
-            key={location.pathname}
-            timeout={450}
-            classNames="fade"
-          >
-            <Switch location={location}>
-              <Route exact path="/portfolio" component={HomePage} />
-              <Route path="/portfolio/about" component={AboutPage} />
-              <Route path="/portfolio/work" component={PortfolioPage} />
-              <Route path="/portfolio/resume" component={ResumePage} />
-              <Route path="/portfolio/contact" component={ContactPage} />
-            </Switch>
-          </CSSTransition>
+          <ModalContext.Provider value={[modalOpen, setModalOpen]}>
+            <AppContext.Provider value={[appTitle, setAppTitle]}>
+              <ShootingStars />
+              <CSSTransition
+                key={location.pathname}
+                timeout={450}
+                classNames="fade"
+              >
+                <Switch location={location}>
+                  <Route exact path="/portfolio" component={HomePage} />
+                  <Route path="/portfolio/about" component={AboutPage} />
+                  <Route path="/portfolio/work" component={PortfolioPage} />
+                  <Route path="/portfolio/resume" component={ResumePage} />
+                  <Route path="/portfolio/contact" component={ContactPage} />
+                </Switch>
+              </CSSTransition>
+            </AppContext.Provider>
+          </ModalContext.Provider>
         </TransitionGroup>
       )}
     />
