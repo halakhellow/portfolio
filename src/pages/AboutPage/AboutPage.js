945f51644b10f6aa2de6c8464cacfd9903dd3522
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-16 02:30:40 +0300

    edit some information in about page

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -13,28 +13,42 @@ let AboutPage = () => {
         <h3>ABOUT ME</h3>
         <div className="AboutPage-info">
           <p>
-            Hey there :) <br /> I'm Hala Alkhellow, a self-taught React
-            developer.
+            Hey there :) <br /> I'm Hala Alkhellow, a Full Stack Developer.
             <br /> I finished university with a bachelor degree in Communication
-            Engineering from Aleppo University. We had some programming subjects
-            and I enjoyed them at that time , so decided to shift career and
-            started studying online with help from professional software
-            engineers throughout mentorship sessions. <br />
+            Engineering from Aleppo University where we received some
+            programming related courses. They were not actual coding courses,
+            but I learned the basics which were enough for my interest to
+            programming! <br /> I decided to shift career and started studying
+            frontend development online with help from professional software
+            engineers throughout mentorship sessions. Then I joined a coding
+            bootcamp to learn Backend web development skills and went a step
+            further toward achieving my dream of being a full stack engineer.
+            <br />
             During this time I made some projects with vanilla JavaScript then
-            started to use React js. You can check them &nbsp;
+            started to code with ReactJS as well as to build APIs. You can check
+            my work &nbsp;
             <Link to="/portfolio/work">here.</Link>
           </p>
           <p>
-            Proficient in Redux js, Hooks, vanilla JavaScript, HTML and CSS ...
-            <br />
+            Technical skills I'm familiar with:
+            <div className="skills">
+              <p className="skill">HTML</p>
+              <p className="skill">CSS</p>
+              <p className="skill">SASS</p>
+              <p className="skill">Bootstrap</p>
+              <p className="skill">JavaScript</p>
+              <p className="skill">ReactJS</p>
+              <p className="skill">NodeJS</p>
+              <p className="skill">ExpressJS</p>
+              <p className="skill">MongoDB</p>
+            </div>
             and other skills mentioned in my &nbsp;
             <Link to="/portfolio/resume">Resume.</Link>
           </p>
 
           <p>
-            I'm an astrophil as you can guess &#127775;, I love looking at the
-            sky and stars, I also enjoy music, watching football, watching tv
-            series, and learning languages.
+            I'm an astrophil as you can guess <i className="fa fa-star" /> , I
+            enjoy music, watching football, and learning languages.
           </p>
           <p>
             Want to connect? <Link to="/portfolio/contact">Let's go!</Link>
@@ -43,11 +57,8 @@ let AboutPage = () => {
             <a href="https://github.com/halakhellow" target="_blank">
               <i className="fab fa-github"></i>
             </a>
-            <a href="https://facebook.com/hallosh" target="_blank">
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a href="https://instagram.com/hall0sh" target="_blank">
-              <i className="fab fa-instagram"></i>
+            <a href="https://codepen.io/halakhellow" target="_blank">
+              <i className="fab fa-codepen"></i>
             </a>
             <a href="https://linkedin.com/in/halakhellow" target="_blank">
               <i className="fab fa-linkedin"></i>
