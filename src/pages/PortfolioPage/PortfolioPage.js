7a22f2f825afb0c18bae6f610a15201f0ddb3498
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-15 16:28:42 +0300

    use the Modal component in PortfolioPage to show details about each application

--- src/pages/PortfolioPage/PortfolioPage.js
+++ src/pages/PortfolioPage/PortfolioPage.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import { nanoid } from "nanoid";
 
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import VanillaJsApplications from "../../components/VanillaJsApplications/VanillaJsApplications";
-import ReactApplications from "../../components/ReactApplications/ReactApplications";
+import Modal from "../../components/Modal/Modal";
+import Application from "../../components/Application/Application";
+
+import { ModalContext } from "../../App";
+import reactAppsInfos from "../../appsDetails/reactAppsDetails";
+import jsAppsDetails from "../../appsDetails/jsAppsDetails";
 
 import "./PortfolioPage.css";
 
 let PortfolioPage = () => {
+  const [modalOpen] = useContext(ModalContext);
   return (
     <div className="PortfolioPage page">
       <div className="page-content">
         <Header />
+        {modalOpen && <Modal />}
         <div className="PortfolioPage-content">
           <h3>Applications with React.js</h3>
-          <ReactApplications />
+          {reactAppsInfos.map((app) => (
+            <Application
+              key={nanoid()}
+              title={app.title}
+              imageSrc={app.imageSrc}
+            />
+          ))}
           <h3>Applications with vanilla JavaScript</h3>
-          <VanillaJsApplications />
+          {jsAppsDetails.map((app, index) => (
+            <Application
+              key={index}
+              title={app.title}
+              imageSrc={app.imageSrc}
+            />
+          ))}
         </div>
       </div>
       <Footer />
