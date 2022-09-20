ec339a31ada637973f18506bda2c5dc8ac576c39
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-20 03:48:57 +0300

    add codepen link to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,9 @@ let Footer = () => {
     <div className="Footer">
       <a href="https://github.com/halakhellow" target="_blank">
         <i className="fab fa-github"></i>
+      </a>{" "}
+      <a href="https://codepen.io/halakhellow" target="_blank">
+        <i className="fab fa-codepen"></i>
       </a>
       <a href="https://linkedin.com/in/halakhellow" target="_blank">
         <i className="fab fa-linkedin"></i>
