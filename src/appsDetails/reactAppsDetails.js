8aec2e3f4e305dc5d6f4747009c4fc5b5b2bb57a
Hala Alkhellow <halakhellow1996@gmail.com>
2022-10-22 17:58:52 +0300

    refactor: delete unused technology

--- src/appsDetails/reactAppsDetails.js
+++ src/appsDetails/reactAppsDetails.js
@@ -6,7 +6,7 @@ const reactAppsDetails = [
     title: "HANGMAN",
     imageSrc: hangmanImg,
     description: `Hangman is a guessing game where you try to guess a word by suggesting letters within a certain number of guesses.`,
-    technologies: ["ReactJS", "TailwindCSS", "WordsAPI"],
+    technologies: ["ReactJS", "TailwindCSS"],
     websiteLink: "https://halakhellow.github.io/hangman/",
     githubLink: "https://github.com/halakhellow/hangman",
   },
