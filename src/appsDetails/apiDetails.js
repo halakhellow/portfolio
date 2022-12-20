a58aa36be5fe7e7df8cb28e4889896b2c11a3ead
Hala Alkhellow <halakhellow1996@gmail.com>
2022-12-20 14:29:19 +0300

    update the deployment link for the cookiez api project

--- src/appsDetails/apiDetails.js
+++ src/appsDetails/apiDetails.js
@@ -7,7 +7,7 @@ const apiDetails = [
     description:
       "API for cookiez website which is a platform to connect customers who are looking for homemade food with chefs who offer that.",
     technologies: ["NodeJS", "ExpressJS", "MongoDB", "Firebase"],
-    websiteLink: "https://cookiez-app.herokuapp.com/api-docs/",
+    websiteLink: "https://cookiez-api.onrender.com/api-docs/",
     githubLink: "https://github.com/halakhellow/cookiez-api",
   },
 ];
