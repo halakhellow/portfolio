3a95601d0b8e557e61574270f47c67cac140ac60
Hala Alkhellow <halakhellow1996@gmail.com>
2023-03-01 11:36:13 +0300

    add the BlogCard for the dynamic programming article

--- src/pages/BlogPage/BlogPage.js
+++ src/pages/BlogPage/BlogPage.js
@@ -4,6 +4,7 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
 import JavaScriptUnderTheHood from "./thumbnails/javascript-under-the-hood.png";
+import dynamicProgramming from "./thumbnails/dynamic-programming.png";
 
 import "./BlogPage.css";
 import BlogCard from "../../components/BlogCard/BlogCard";
@@ -16,16 +17,26 @@ let BlogsPage = () => {
         <h3>
           Check my published articles on <span>Medium</span> platform
         </h3>
-        <BlogCard
-          imgSrc={JavaScriptUnderTheHood}
-          title="JavaScript Under The Hood"
-          description="The core concepts behind JavaScript programming language
+        <div className="blogs">
+          <BlogCard
+            imgSrc={JavaScriptUnderTheHood}
+            title="JavaScript Under The Hood"
+            description="The core concepts behind JavaScript programming language
                 including the Call Stack, Execution Context, Event Loop,
                 JIT(Just In Time) Compiler, and JS Engine."
-          link="https://medium.com/@halakhellow/javascript-under-the-hood-623add30830c"
-          time="6"
-          date="November 28th, 2022"
-        />
+            link="https://medium.com/@halakhellow/javascript-under-the-hood-623add30830c"
+            time="6"
+            date="November 28th, 2022"
+          />
+          <BlogCard
+            imgSrc={dynamicProgramming}
+            title="Dynamic Programming"
+            description="The concept of dynamic programming and its applications in problem-solving. Demonstrating its implementation on the classic Fibonacci problem, using both memoization and tabulation techniques."
+            link="https://medium.com/@halakhellow/dynamic-programming-memoization-vs-tabulation-5d1ee8075327"
+            time="4"
+            date="February 27th, 2023"
+          />
+        </div>
       </div>
       <Footer />
     </div>
