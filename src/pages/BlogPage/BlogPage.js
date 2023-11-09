5ffa36b34c79f54117601e276f074af7dbd4bd91
Hala Alkhellow <halakhellow1996@gmail.com>
2023-11-09 16:52:46 +0300

    add new components for the new blog posts

--- src/pages/BlogPage/BlogPage.js
+++ src/pages/BlogPage/BlogPage.js
@@ -5,6 +5,8 @@ import Footer from "../../components/Footer/Footer";
 
 import JavaScriptUnderTheHood from "./thumbnails/javascript-under-the-hood.png";
 import dynamicProgramming from "./thumbnails/dynamic-programming.png";
+import etlElt from "./thumbnails/etl-elt.png";
+import makeAutomation from "./thumbnails/make-automation.png";
 
 import "./BlogPage.css";
 import BlogCard from "../../components/BlogCard/BlogCard";
@@ -35,6 +37,22 @@ let BlogsPage = () => {
             link="https://medium.com/@halakhellow/dynamic-programming-memoization-vs-tabulation-5d1ee8075327"
             time="4"
             date="February 27th, 2023"
+          />
+          <BlogCard
+            imgSrc={etlElt}
+            title="ETL vs. ELT in Data Engineering"
+            description="Two acronyms often pop up In the ever-evolving landscape of data engineering: ETL (Extract, Transform, Load) and ELT (Extract, Load, Transform). These processes are essential in the data journey from source to analysis, shaping the foundation of data-driven decision-making."
+            link="https://medium.com/@halakhellow/etl-vs-elt-in-data-engineering-extract-transform-load-5f3fafeac576"
+            time="5"
+            date="September 15th, 2023"
+          />
+          <BlogCard
+            imgSrc={makeAutomation}
+            title="Embracing the Magic of Automation with Make Platform"
+            description="In the world of automation, the Make platform stands as a strong supporter, empowering individuals and organizations to streamline their tasks, boost productivity, and eliminate the need for repetitive manual work."
+            link="https://medium.com/@halakhellow/embracing-the-magic-of-automation-with-make-platform-6774af24d6da"
+            time="4"
+            date="October 12th, 2023"
           />
         </div>
       </div>
